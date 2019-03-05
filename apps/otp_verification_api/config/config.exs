4a94c7b9274041ccfd4d67703dd5d8d89c76b642
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-03-05 20:47:14 +0200

    feat: phoenix instruments (#71)

--- apps/otp_verification_api/config/config.exs
+++ apps/otp_verification_api/config/config.exs
@@ -4,6 +4,7 @@ use Mix.Config
 config :otp_verification_api, OtpVerification.Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "7X/pAxXDa89ArqDGkBZCS4eTGKGDOdx1DDDWKS/AB42isRqbn0LoZXhIQ2n/XqTK",
-  render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)]
+  render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)],
+  instrumenters: [LoggerJSON.Phoenix.Instruments]
 
 import_config "#{Mix.env()}.exs"
--- apps/otp_verification_api/config/dev.exs
+++ apps/otp_verification_api/config/dev.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :otp_verification_api, OtpVerification.Web.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
